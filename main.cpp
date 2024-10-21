8c90c8923b1dda98e8cf3b166b2792ecef4630ae
Kendra Devlahovicht <178851939+k7ndr4@users.noreply.github.com>
2024-10-21 16:24:49 -0700

    fixed formatting & added vars for controls

--- main.cpp
+++ main.cpp
@@ -21,6 +21,11 @@ int main(int argc, char** argv) {
     const int WIN_CON_EASY = 65; //NUM OF POINTS TO WIN THE GAME (easy)
     const int WIN_CON_MED  = 100; //NUM OF POINTS TO WIN THE GAME (medium)
     const int WIN_CON_HARD = 150; //NUM OF POINTS TO WIN THE GAME (hard)
+    
+    //CONTROLS
+    const char START_GAME_BTN = '1';
+    const char EXIT_GAME_BTN  = '3';
+    
     const int BAD_NUM   = 1; //THE SIDE OF THE DIE THAT WILL RESULT IN ENDING YOUR TURN
     const int DIE_SIDES = 6; //AMOUNT OF SIDES ON THE DIE
     unsigned int points = 0; //AMOUNT OF CURRENT POINTS
@@ -30,11 +35,11 @@ int main(int argc, char** argv) {
     //EXPLAIN RULES
     char input;
     cout << "Welcome to the game of Pig Dice!\n" <<
-            "To play the game, you roll a die as many times as you'd like to try and reach \n" << 
-            "EASY: " << WIN_CON_EASY << " points\n MEDIUM: " << WIN_CON_MED << " points\n HARD: " << WIN_CON_HARD << " points\n in a minimum amount of turns.\n" <<
-            "The catch is, if you roll a " << BAD_NUM << " you will lose all your points, and must start again.\n" <<
+            "To play the game, you roll a die as many times as you'd like to try and reach \n\n" << 
+            "EASY: " << WIN_CON_EASY << " points\nMEDIUM: " << WIN_CON_MED << " points\nHARD: " << WIN_CON_HARD << " points\n\nin a minimum amount of turns.\n" <<
+            "The catch is, if you roll a " << BAD_NUM << " your points will revert back to the amount of points you had since the last time you chose to end a turn manually, and the amount of turns will increase by 1.\n" <<
             "You may end your turn at any time to save the number of points you have, and try to minimize the amount of turns until you reach the desired amount of points.\n" <<
-            "Type '1' to start the game, or type '3' to exit the program.\n";
+            "Type " << START_GAME_BTN <<  " to start the game, or type " << EXIT_GAME_BTN << " to exit the program.\n";
     
     //CAPTURE USER INPUT & START THE GAME IF REQUESTED
     cin >> input;
@@ -77,6 +82,7 @@ int main(int argc, char** argv) {
     cout << "\nSTARTING GAME\n\n";
     
     char roll;
+    int pointCheckpoint;
     while(points < currWinCon){
         cout << "Roll the die by inputting 'R', or input anything else to stop your turn.\n";
         cin >> roll;
@@ -87,7 +93,7 @@ int main(int argc, char** argv) {
             cout << "You rolled a " << currRolled << '\n';
 
             if(currRolled == 1){
-                points = 0;
+                points = pointCheckpoint;
                 cout << "You have lost all your points. You are currently on turn: " << currTurn++ << '\n';
                 cout << "\nTURN: " << currTurn << '\n';
             }else{
@@ -97,6 +103,7 @@ int main(int argc, char** argv) {
         }else{
             cout << "You have chosen to end your turn. It is currently turn " << currTurn++ <<
                     "\nYou have " << points << " amount of points.\n\n";
+            pointCheckpoint = points;
         }
     }
     
