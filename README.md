af42c05e08008984f8e785942fb1b115def33363
Kendra Devlahovicht <178851939+k7ndr4@users.noreply.github.com>
2024-10-21 15:19:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-1-CIS5
+ C++ Game Project for CIS 5
---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Kendra
+ *
+ * Created on October 21, 2024, 3:16 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
