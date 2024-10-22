fc6b8791ad9a49768085126154e4373376c13d0d
Kendra Devlahovicht <178851939+k7ndr4@users.noreply.github.com>
2024-10-21 17:13:54 -0700

    final changes

--- main.cpp
+++ main.cpp
@@ -37,116 +37,118 @@ int main(int argc, char** argv) {
     
     //EXPLAIN RULES
     char input;
-    cout << "Welcome to the game of Pig Dice!\n" <<
-            "To play the game, you roll a die as many times as you'd like to try and reach \n\n" << 
-            "EASY: " << WIN_EASY << " points\nMEDIUM: " << WIN_MED << " points\nHARD: " << WIN_HARD << " points\n\nin a minimum amount of turns.\n" <<
-            "The catch is, if you roll a " << BAD_NUM << " your points will revert back to the amount of points you had since the last time you chose to end a turn manually, and the amount of turns will increase by 1.\n" <<
-            "You may end your turn at any time to save the number of points you have, and try to minimize the amount of turns until you reach the desired amount of points.\n" <<
-            "Type " << STRT_BTN <<  " to start the game, or type " << EXIT_BTN << " to exit the program.\n\n";
-    
-    //CAPTURE USER INPUT & START THE GAME IF REQUESTED
-    cin >> input;
-    if(input == EXIT_BTN){
-        cout << "Exiting game...\n";
-        exit(0);
-    }else if(input == STRT_BTN){
-        //DO NOTHING, CONTINUE ON WITH PROGRAM
-    }else{
-        cout << "Exiting game...\n";
-        exit(0);
-    }
-    
-    //CHOOSE DIFFICULTY
-    char diff;
-    string diffStr;
-    int currCon;
-    cout << "Please choose your difficulty...\n" <<
-            "Enter 'E' for Easy   (" << WIN_EASY <<  " points)\n" <<
-            "Enter 'M' for Medium (" << WIN_MED  <<  " points)\n" <<
-            "Enter 'H' for Hard   (" << WIN_HARD <<  " points)\n";
-    cin >> diff;
-    
-    switch(diff){
-        case 'E': 
-            currCon = WIN_EASY;
-            diffStr = "EASY";
-            break;
-        
-        case 'M': 
-            currCon = WIN_MED;
-            diffStr = "MEDIUM";
-            break;
-            
-        case 'H': 
-            currCon = WIN_HARD;
-            diffStr = "HARD";
-            break;
-            
-        default : 
-            currCon = WIN_EASY;
-            diffStr = "EASY";
-            break;
-    }
-    
-    
-    //START THE GAME & CREATE A TEXT FILE
-    ofstream MyFile("gameLogs.txt");
-    MyFile << "\nGame Session Started\n";
-    
-    cout << "\nSTARTING GAME on " <<  diffStr << " DIFFICULTY\n\n";
-    
-    //GAME LOOP
-    char roll;
-    int cPoint = 0;
-    do{
-        cout << "Roll the die by inputting 'R', or input anything else to stop your turn.\n";
-        cin >> roll;
-        
-    //IF THE PLAYER CHOOSES TO CONTINUE ROLLING THE DIE
-        if(roll == 'R'){
-            
-            //ROLL THE DIE & OUTPUT THE RESULT
-            currRoll = rand()%DIE_SIDES+1;
-            cout << "You rolled a " << currRoll << '\n';
+    while(input != EXIT_BTN){
+        cout << "Welcome to the game of Pig Dice!\n" <<
+                "To play the game, you roll a die as many times as you'd like to try and reach \n\n" << 
+                "EASY: " << WIN_EASY << " points\nMEDIUM: " << WIN_MED << " points\nHARD: " << WIN_HARD << " points\n\nin a minimum amount of turns.\n" <<
+                "The catch is, if you roll a " << BAD_NUM << " your points will revert back to the amount of points you had since the last time you chose to end a turn manually, and the amount of turns will increase by 1.\n" <<
+                "You may end your turn at any time to save the number of points you have, and try to minimize the amount of turns until you reach the desired amount of points.\n" <<
+                "Type " << STRT_BTN <<  " to start the game, or type " << EXIT_BTN << " to exit the program.\n\n";
 
-            //IF THE DIE ROLL LANDED ON '1' THEN THE PLAYER LOSES THEIR POINTS AND THE TURN # INCREASES
-            if(currRoll == 1){
-                points = cPoint;
-                cout << "Your points have been reverted to " << cPoint << " points. You are currently on turn: " << currTurn++ << '\n';
-                cout << "\nTURN: " << currTurn << '\n';
-            }else{
-                points += float(currRoll);
-                cout << "Your current amount of points are: " << points << '\n';
-            }
-    //IF THE PLAYER CHOOSES TO MANUALLY END THE TURN
-        }else{
-            cout << fixed << setprecision(0) << 
-                    "You have chosen to end your turn. It is currently turn " << currTurn++ <<
-                    "\nYou have " << points << " amount of points.\n\n";
-            cPoint = points;
+        //CAPTURE USER INPUT & START THE GAME IF REQUESTED
+          cin >> input;
+    //    if(input == EXIT_BTN){
+    //        cout << "Exiting game...\n";
+    //        exit(0);
+    //    }else if(input == STRT_BTN){
+    //        //DO NOTHING, CONTINUE ON WITH PROGRAM
+    //    }else{
+    //        cout << "Exiting game...\n";
+    //        exit(0);
+    //    }
+
+        //CHOOSE DIFFICULTY
+        char diff;
+        string diffStr;
+        int currCon;
+        cout << "Please choose your difficulty...\n" <<
+                "Enter 'E' for Easy   (" << WIN_EASY <<  " points)\n" <<
+                "Enter 'M' for Medium (" << WIN_MED  <<  " points)\n" <<
+                "Enter 'H' for Hard   (" << WIN_HARD <<  " points)\n";
+        cin >> diff;
+
+        switch(diff){
+            case 'E': 
+                currCon = WIN_EASY;
+                diffStr = "EASY";
+                break;
+
+            case 'M': 
+                currCon = WIN_MED;
+                diffStr = "MEDIUM";
+                break;
+
+            case 'H': 
+                currCon = WIN_HARD;
+                diffStr = "HARD";
+                break;
+
+            default : 
+                currCon = WIN_EASY;
+                diffStr = "EASY";
+                break;
         }
-    }while(points < currCon);
-    
-    //ENSURE THAT THE WHILE LOOP BROKE BECAUSE PLAYER REACHED WIN CONDITION
-    bool beatGame = points >= currCon;
-    if(beatGame){
-        cout << "Congratulations! You have beat the game on " << diffStr << " difficulty, in " << currTurn << " turns!\n";
+
+
+        //START THE GAME & CREATE A TEXT FILE
+        ofstream MyFile("gameLogs.txt");
+        MyFile << "\nGame Session Started\n";
+
+        cout << "\nSTARTING GAME on " <<  diffStr << " DIFFICULTY\n\n";
+
+        //GAME LOOP
+        char roll;
+        int cPoint = 0;
+        do{
+            cout << "Roll the die by inputting 'R', or input anything else to stop your turn.\n";
+            cin >> roll;
+
+        //IF THE PLAYER CHOOSES TO CONTINUE ROLLING THE DIE
+            if(roll == 'R'){
+
+                //ROLL THE DIE & OUTPUT THE RESULT
+                currRoll = rand()%DIE_SIDES+1;
+                cout << "You rolled a " << currRoll << '\n';
+
+                //IF THE DIE ROLL LANDED ON '1' THEN THE PLAYER LOSES THEIR POINTS AND THE TURN # INCREASES
+                if(currRoll == 1){
+                    points = cPoint;
+                    cout << "Your points have been reverted to " << cPoint << " points. You are currently on turn: " << currTurn++ << '\n';
+                    cout << "\nTURN: " << currTurn << '\n';
+                }else{
+                    points += float(currRoll);
+                    cout << "Your current amount of points are: " << points << '\n';
+                }
+        //IF THE PLAYER CHOOSES TO MANUALLY END THE TURN
+            }else{
+                cout << fixed << setprecision(0) << 
+                        "You have chosen to end your turn. It is currently turn " << currTurn++ <<
+                        "\nYou have " << points << " amount of points.\n\n";
+                cPoint = points;
+            }
+        }while(points < currCon);
+
+        //ENSURE THAT THE WHILE LOOP BROKE BECAUSE PLAYER REACHED WIN CONDITION
+        bool beatGame = points >= currCon ? true : false;
+        if(beatGame){
+            cout << "Congratulations! You have beat the game on " << diffStr << " difficulty, in " << currTurn << " turns!\n\n";
+        }
+
+        //RECORD DATA IN FILE
+        const int DATANUM = 2;
+
+        for(int i = 0; i < DATANUM; ++i){
+            string output;
+
+            if(i == 0) output = "Difficulty: " + diffStr + "\n";
+            else if(i == 1) output = "Amt of Turns: " + to_string(currTurn) + "\n";
+            else output = "---------\n\n";
+
+            MyFile << output;
+        }
+
+        MyFile.close();
     }
-    
-    //RECORD DATA IN FILE
-    const int DATANUM = 2;
-    
-    for(int i = 0; i < DATANUM; ++i){
-        string output;
-        
-        if(i == 0) output = "Difficulty: " + diffStr + "\n";
-        else if(i == 1) output = "Amt of Turns: " + to_string(currTurn) + "\n";
-        else output = "---------\n\n";
-        
-        MyFile << output;
-    }
-            
-    MyFile.close();
     
     return 0;
 }
